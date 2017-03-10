dfb23d28efc3f085e6b3ddbd97a1a2ef4501152d
hanscoder <hanssowa@gmail.com>
2017-03-10 19:18:25 +0100

    feat(index): Mark dependencies as release or non release in the dependency section of a given packag

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = () => {
-    console.log("Halli Hallo");
+import {toPairs, flow, map} from 'lodash/fp';
+import {clean} from "semver";
+
+export function dependencyTreeLint(packageJson) {
+    let dependencies = toPairs(packageJson["dependencies"]);
+
+    return flow(
+        map(pair => {
+            const [name, version] = pair;
+            return new Dependency(name, version, toVersionType(version));
+        })
+    )(dependencies);
+}
+
+function toVersionType(version) {
+    return clean(version.trim()) !== null ? 'RELEASE' : 'NON-RELEASE';
+}
+
+export class Dependency {
+
+    constructor(name, version, state) {
+        this.name = name;
+        this.version = version;
+        this.state = state;
+    }
 }
---
+++ src/index.test.js
@@ -0,0 +1,59 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import {dependencyTreeLint, Dependency} from './index.js';
+
+const ONE_DEPENDENCY_WITH_FIXED_VERSION = new Dependency("dependency1", "1.1.1", 'RELEASE');
+const ONE_DEPENDENCY_WITH_NON_FIXED_VERSION = new Dependency("dependency1", "^1.1.1", 'NON-RELEASE');
+const DEPENDENCYIES_WITH_DEPENDENCIES_WITH_WITH_FIXED_AND_NON_FIXED_VERSION = [
+    new Dependency("dependency1", "^1.1.1", 'NON-RELEASE'),
+    new Dependency("dependency2", "1.1.1", 'RELEASE'),
+    new Dependency("dependency3", "2.1.1", 'RELEASE'),
+    new Dependency("dependency4", "1.1.x", 'NON-RELEASE'),
+];
+
+
+const PACKAGE_JSON_WITH_ONE_DEPENDENCY_WITH_ONE_FIXED_VERSION = {
+    "dependencies": {
+        "dependency1": "1.1.1",
+    }
+};
+
+const PACKAGE_JSON_WITH_ONE_DEPENDENCY_WITH_NON_FIXED_VERSION = {
+    "dependencies": {
+        "dependency1": "^1.1.1",
+    }
+};
+
+const PACKAGE_JSON_WITH_DEPENDENCIES_WITH_WITH_FIXED_AND_NON_FIXED_VERSION = {
+    "dependencies": {
+        "dependency1": "^1.1.1",
+        "dependency2": "1.1.1",
+        "dependency3": "2.1.1",
+        "dependency4": "1.1.x",
+    }
+};
+
+describe('dependency-tree-lint', () => {
+
+    it('should mark one dependency as release', () => {
+        let dependencies = dependencyTreeLint(PACKAGE_JSON_WITH_ONE_DEPENDENCY_WITH_ONE_FIXED_VERSION);
+
+        expect(dependencies).to.include(ONE_DEPENDENCY_WITH_FIXED_VERSION);
+        expect(dependencies).to.have.length(1);
+    });
+
+    it('should mark one dependency as non release', () => {
+        let dependencies = dependencyTreeLint(PACKAGE_JSON_WITH_ONE_DEPENDENCY_WITH_NON_FIXED_VERSION);
+
+        expect(dependencies).to.include(ONE_DEPENDENCY_WITH_NON_FIXED_VERSION);
+        expect(dependencies).to.have.length(1);
+    });
+
+    it('should mark more dependencies as release or non-release', () => {
+        let dependencies = dependencyTreeLint(PACKAGE_JSON_WITH_DEPENDENCIES_WITH_WITH_FIXED_AND_NON_FIXED_VERSION);
+
+        expect(dependencies).to.deep.include.members(DEPENDENCYIES_WITH_DEPENDENCIES_WITH_WITH_FIXED_AND_NON_FIXED_VERSION);
+        expect(dependencies).to.have.length(4);
+    });
+
+});
